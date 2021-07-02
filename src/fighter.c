5033433456689797c418e5e7b7576e4961262185
Ignacio Oyarzabal <ignaoya93@gmail.com>
2021-07-02 12:50:05 +0200

    fixed minor messaging bug

--- src/fighter.c
+++ src/fighter.c
@@ -6,9 +6,9 @@ void attack(Fighter* attacker, Fighter* defender)
 	int damage = maxInt(0, attacker->attack - defender->defense);
 	char* att_name = attacker->owner->name;
 	char* def_name = defender->owner->name;
-	takeDamage(defender, damage);
 	snprintf(text, sizeof(text), "The %s attacks the %s for %i damage!", att_name, def_name, damage); 
 	addMessage(text);
+	takeDamage(defender, damage);
 }
 
 
